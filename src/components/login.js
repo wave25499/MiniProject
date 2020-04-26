8a25754d6952c92bd0032df9b2bff35326d8d4da
wave25499 <wave_vongola10@hotmail.com>
2020-04-26 17:40:44 +0700

    Seventh

--- src/components/login.js
+++ src/components/login.js
@@ -53,11 +53,7 @@ const Login =({ setSession })=>{
   const uiConfig = {
     signInFlow: "popup",
     signInOptions: [
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        firebase.auth.GithubAuthProvider.PROVIDER_ID,
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
     ],
     callbacks: {
       signInSuccess: () => false
